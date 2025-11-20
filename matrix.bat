f494239a00c8ae6cd1089338f872047738523b1d
DAN609 <159565306+daniil609666@users.noreply.github.com>
2025-11-20 11:11:18 +0300

    Add files via upload

--- matrix.bat
+++ matrix.bat
@@ -2,6 +2,11 @@
 color 2
 title hack3d
 :a
+<<<<<<< HEAD
+echo %random% %random% %random% %random% %random% %random% %random%
+echo %random% %random% %random% %random% %random% %random% %random% %random%
+=======
 echo %ramdom% %ramdom%%ramdom% %ramdom% 
 echo %ramdom% %ramdom%%ramdom%
+>>>>>>> 890e0533ff875f7213e555eab3b9d56197fb25d8
 goto :a
